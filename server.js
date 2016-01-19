558982d95eb76f66898335b3b9f592109343d944
shaykyasin <shayk.yasin@gmail.com>
2016-01-19 20:21:41 +0530

    Fixing port and urls

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var server = http.createServer(function (req, res) {
 		}
 	}
 });
-server.listen(8080);
+server.listen((process.env.PORT || 5000));
 
 function getIso(val) {
 	val = parseInt(val);
